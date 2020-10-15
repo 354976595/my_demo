6f80bc74cb9294f3ab7271e28cab2a93adb7fe67
xujiadongdev <xujiadongdev@gmail.com>
2020-10-15 11:04:29 +0800

    aspect

--- springdemo/src/main/java/com/example/springdemo/aop/ProjAspect.java
+++ springdemo/src/main/java/com/example/springdemo/aop/ProjAspect.java
@@ -1,9 +1,7 @@
 package com.example.springdemo.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -17,12 +15,16 @@ public class ProjAspect  {
     @Around(value = "asp()")
     public Object arg
             (ProceedingJoinPoint joint) throws Throwable {
+        //字段类型
         for (Field declaredField : joint.getSignature().getDeclaringType().getDeclaredFields()) {
             System.out.println("felid class name -------"+declaredField.getType().getName());
         }
         if(joint.getArgs()==null){
            throw new Exception("cnmmmlssdee");
         }
+        /**
+         * 切面函数的参数
+         */
         for (Object arg : joint.getArgs()) {
             if(null!=arg) {
                 System.out.println(arg.getClass().getName());
@@ -30,12 +32,18 @@ public class ProjAspect  {
             }
 
         }
-
         logger.info(ProjAspect.class.getName()+"asp log**************");
         return joint.proceed();
 
     }
-
+    @Before("execution(* com.example.springdemo.service..*.*(..))")
+    public void before(){
+        System.out.println("before aop-------------");
+    }
+    @After("execution(* com.example.springdemo.service..*.*(..))")
+    public void After(){
+        System.out.println("After aop-------------");
+    }
     @Pointcut(value = "execution(* com.example.springdemo.service..*.*(..))")
     public void asp(){
 
